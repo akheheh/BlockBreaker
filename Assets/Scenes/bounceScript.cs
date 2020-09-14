054cfd2da7a8fe8dc409839a796b3013d2234d4d
Akheheh <lx.heidarian@gmail.com>
2020-09-14 10:05:40 -0700

    Added sounds and a "sound lab" scene for experimenting

---
+++ Assets/Scenes/bounceScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounceScript : MonoBehaviour
+{
+    [SerializeField] AudioClip soundSource;
+    private void OnCollisionEnter2D(Collision2D obj) {
+        GetComponent<AudioSource>().PlayOneShot(soundSource);
+    }
+}
